fef22fcaccd7b52f5952b0765db49d0d6d43dc7e
ricalizania <lizaniarica@gmail.com>
2024-09-20 13:00:41 +0700

    tugas3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/user/profile', [UserController::class, 'profile']);
+Route::get('/user/create', [UserController::class, 'create']);
+Route::post('/user/store', [UserController::class, 'store']) ->name('user/store');
